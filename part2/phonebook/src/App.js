d64d8c6c92c355061aa092eda24523c04fcd4c4a
Bohdan Mukha <by.muha@gmail.com>
2023-02-07 22:34:38 +0200

    exercise 2.9* done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,24 +2,43 @@ import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '+38 (050) 433-73-28' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ]);
+
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const handleAddButtonClick = (event) => {
     event.preventDefault();
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons([...persons, { name: newName, number: newNumber }]);
+      setPersons([
+        ...persons,
+        { name: newName, number: newNumber, id: persons.length + 1 },
+      ]);
     }
     setNewName('');
     setNewNumber('');
   };
 
+  const personsToRender = filter
+    ? persons.filter((person) => person.name.toLowerCase().includes(filter))
+    : persons;
+
   return (
     <div>
+      <div>
+        filter shown with:
+        <input
+          value={filter}
+          onChange={(event) => setFilter(event.target.value)}
+        />
+      </div>
       <h2>Phonebook</h2>
       <form>
         <div>
@@ -43,8 +62,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>
+      {personsToRender.map((person) => (
+        <p key={person.id}>
           {person.name} {person.number}
         </p>
       ))}
