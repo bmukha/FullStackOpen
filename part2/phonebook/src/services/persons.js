28187fbc9cc4dc671b985719db298a9df75eda5a
Bohdan Mukha <by.muha@gmail.com>
2023-02-13 16:57:16 +0200

    exercise 3.11 done

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,17 +1,14 @@
 import axios from 'axios';
 
-const url = 'http://localhost';
-const port = '3001';
+const url = 'api/persons';
 
-const getAll = () => axios.get(`${url}:${port}/api/persons`);
+const getAll = () => axios.get(url);
 
-const addPerson = (newPerson) =>
-  axios.post(`${url}:${port}/api/persons`, newPerson);
+const addPerson = (newPerson) => axios.post(url, newPerson);
 
-const deletePerson = (id) => axios.delete(`${url}:${port}/api/persons/${id}`);
+const deletePerson = (id) => axios.delete(`${url}/${id}`);
 
-const updatePersonsNumber = (id, person) =>
-  axios.put(`${url}:${port}/api/persons/${id}`, person);
+const updatePersonsNumber = (id, person) => axios.put(`${url}/${id}`, person);
 
 const personService = { getAll, addPerson, deletePerson, updatePersonsNumber };
 
